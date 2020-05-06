b0be3cc9ce90b205cac34fc973d6311bdc878f81
Magicallly <63975139+Magicallly@users.noreply.github.com>
2020-05-06 23:47:10 +0300

    Add files via upload

---
+++ lab7.dpr
@@ -0,0 +1,89 @@
+﻿program lab7;
+
+{$APPTYPE CONSOLE}
+{$R *.res}
+
+uses SysUtils;
+
+var
+  s, str1: string;
+  z, data: boolean;
+  max_day, i, k, g, month, day, year: integer;
+
+begin
+  write('Введите строку: ');
+  Readln(s);
+  if length(s) = 10 then
+  begin
+    for g := 1 to 2 do    // loop A
+    begin
+      str1 := str1 + s[g];
+    end;
+    day := StrToInt(str1);
+    delete(str1, 1, length(str1));
+
+    for g := 4 to 5 do       // loop B
+    begin
+      str1 := str1 + s[g];
+    end;
+    month := StrToInt(str1);
+    delete(str1, 1, length(str1));
+
+    for g := 7 to 10 do     // loop C
+    begin
+      str1 := str1 + s[g];
+    end;
+    year := StrToInt(str1);
+
+    data := (length(s) = 10) and (s[3] = '.') and (s[6] = '.') and (month >= 1) and (month <= 12);
+    if data then
+    begin
+      case month of
+        02:
+          if (year mod 400 = 0) or ((year mod 4 = 0) and (year mod 100 <> 0))
+          then
+            max_day := 29
+          else
+            max_day := 28;
+        04, 06, 09, 11:
+          max_day := 30
+      else
+        max_day := 31
+      end;
+
+      data := (day >= 01) and (day <= max_day) and (month >= 1) and (month <= 12);
+      if data then
+
+        writeln('Вы ввели верную дату.')
+        else writeln('Вы ввели неверную дату.')
+    end
+      else writeln('Вы ввели неверную дату.')
+
+  end
+  else
+  begin
+    k := 0;
+    z := True;
+    for i := 1 to length(s) do   // loop D
+    begin
+      if s[i] = '.' then
+        inc(k);
+      if not((s[i] in ['0' .. '9']) or (s[i] in ['.', '-'])) or (k > 1) or
+        ((s[i] = '-') and (i <> 1)) then
+        z := False;
+    end;
+    if z then
+    begin
+      writeln('Строка является числом.');
+      if Pos('.', s) > 0 then
+        writeln('Число вещественное.')
+      else
+        writeln('Число целое.');
+    end
+    else
+      writeln('Строка не является числом!');
+  end;
+
+  Readln;
+
+end.
---
+++ lab7_1.dpr
@@ -0,0 +1,53 @@
+﻿program lab7_1;
+
+{$APPTYPE CONSOLE}
+{$R *.res}
+
+Uses SysUtils;
+
+Const
+  abc = 'abcdefghijklmnopqrstuvwxyz';
+  vowel = ['a', 'e', 'i', 'o', 'u', 'y'];
+
+Var
+  st, endword, word, st2: string;
+  i: integer;
+
+begin
+  write('Введите строку: ');
+  readln(st);
+  st := trim(st);
+  for i := 1 to length(st) do   // loopA
+  begin
+    while ((st[i] = ' ') and (st[i + 1] = ' ')) do   // loopB
+      delete(st, i + 1, 1);
+  end;
+  st2 := copy(st,1,length(st));
+  for i := 1 to length(st2) do    // loopC
+  begin
+    if st2[i] in vowel then
+      delete(st2, i, 1);
+  end;
+  if st2='' then writeln ('Пустая строка')
+  else begin
+  Writeln('Строка, без гласных букв: ');
+  Writeln(st2);
+  end;
+
+  while length(st) > 1 do          // loopD
+  begin
+    word := Copy(st, 1, Pos(' ', st) - 1);
+    delete(st, 1, Pos(' ', st));
+    if (word <> endword) and (Pos(word, abc) = length(abc) - length(word) + 1)
+    then Write(word, ' ');
+    word:=word+' '+word;
+  end;
+  if word='' then  writeln ('Пустая строка')
+   else begin
+  Writeln('Слова, совпадающие с конечным отрезком латинского алфавита: ');
+  Writeln(word);
+  end;
+
+
+  readln;
+end.
